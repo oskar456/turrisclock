22b7a44ef69831c0ab283bd6e05eb7aa17e13065
Ondřej Caletka <ondrej@caletka.cz>
2014-10-26 17:00:03 +0100

    New simple executable goto.py <hh:mm:ss>

---
+++ goto.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python2.7
+# vim: expandtab shiftwidth=4 tabstop=8 softtabstop=4 smartindent cinwords=if,elif,else,for,while,try,except,finally,def,class,with
+
+from turrisclock import Clock, argument_parser, clockinit
+
+if __name__ == "__main__":
+
+
+    parser = argument_parser(description='TurrisClock goto')
+    parser.add_argument("destination", help='hh:mm:ss to reach')
+    args = parser.parse_args()
+    clock, statestore = clockinit(args)
+    deststate = Clock.parsestate(args.destination)
+
+    try:
+        while clock.state != deststate:
+            clock.step()
+    finally:
+        statestore.save()
