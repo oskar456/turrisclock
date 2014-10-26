13cb3ec23b7b21c319892f7bcf7b3d4a1de4d200
Ondřej Caletka <ondrej@caletka.cz>
2014-10-26 16:59:41 +0100

    Factor common functions out of turrisclock main

--- turrisclock.py
+++ turrisclock.py
@@ -6,52 +6,57 @@ from clock import Clock
 from statestore import StateStore
 import signal
 import time
+import argparse
 
+def killhandler(signum, frame):
+    raise RuntimeError("Killed")
 
-if __name__ == "__main__":
+signal.signal(signal.SIGTERM, killhandler)
 
-    def killhandler(signum, frame):
-        raise RuntimeError("Killed")
+def pos_int(value):
+    ivalue = int(value)
+    if ivalue <= 0:
+        raise argparse.ArgumentTypeError("{} is an invalid positive int value".format(value))
+    return ivalue
 
-    import argparse
-
-    def pos_int(value):
-        ivalue = int(value)
-        if ivalue <= 0:
-            raise argparse.ArgumentTypeError("{} is an invalid positive int value".format(value))
-        return ivalue
-
-    parser = argparse.ArgumentParser(description='TurrisClock')
+def argument_parser(description='TurrisClock'):
+    parser = argparse.ArgumentParser(description=description)
     parser.add_argument("--invert", action='store_true')
     parser.add_argument("--uninvert", action='store_true')
     parser.add_argument("--state", help='hh:mm:ss')
-    parser.add_argument("--step", "-s", help='step interval', default=1, type=pos_int)
-    parser.add_argument("--comfortstep", "-c", help='comfort step interval when waiting', default=10, type=pos_int)
-    args = parser.parse_args()
+    return parser
 
-
+def clockinit(args):
     CLK = GPIO("224", "out")
     POL = GPIO("225", "out")
     clock = Clock(CLK, POL)
     statestore = StateStore(clock)
     statestore.restore()
-
     if args.invert:
         clock.inverse = True
-
     if args.uninvert:
         clock.inverse = False
-
     if args.state:
         clock.setState(args.state)
+    return (clock, statestore)
 
-    signal.signal(signal.SIGTERM, killhandler)
+if __name__ == "__main__":
+
+
+    parser = argument_parser(description='TurrisClock')
+    parser.add_argument("--step", "-s", help='step interval', default=1, type=pos_int)
+    parser.add_argument("--comfortstep", "-c", help='comfort step interval when waiting', default=10, type=pos_int)
+    args = parser.parse_args()
+    if args.step > args.comfortstep:
+        args.comfortstep = args.step
+    clock, statestore = clockinit(args)
 
     try:
         while True:
             now = time.time()
             nows = time.localtime(now)
             nowstate = Clock.hourstostate(nows.tm_hour, nows.tm_min, nows.tm_sec)
+            nowstate -= nowstate%args.step
             # In case clock is too fast, it's wise to wait a bit
             towait = clock.timetowait(nowstate, args.comfortstep) \
                      + args.step - now%args.step
